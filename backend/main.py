169eebf80406cc474582c48166e3ac85fc6d102f
Ananya Battineni <ananyarjun2005@gmail.com>
2026-02-04 13:17:32 +0530

    cors credentials = false

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
         "https://esw-frontend.vercel.app"
         "https://esw-azure.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
